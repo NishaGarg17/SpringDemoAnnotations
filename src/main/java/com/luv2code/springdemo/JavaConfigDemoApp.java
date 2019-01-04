1025e1c07d4acee5447e8e231d74b06595ada4b5
Nisha <14gargnisha>
2019-01-04 08:47:30 +0530

    commit Spring Demo Annotations V4

--- src/main/java/com/luv2code/springdemo/JavaConfigDemoApp.java
+++ src/main/java/com/luv2code/springdemo/JavaConfigDemoApp.java
@@ -1,22 +1,20 @@
 package com.luv2code.springdemo;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.luv2code.config.SportConfig;
 
-public class JavaConfigDemoApp 
-{
-    public static void main( String[] args )
-    {
-    	//ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
-        //get the bean from spring container
-        Coach theCoach = context.getBean("tennisCoach", Coach.class);
-        //call a method on the bean
-        System.out.println(theCoach.getDailyWorkout());
-        System.out.println(theCoach.getDailyFortune());
-        //Close the context
-        context.close();
-    }
+public class JavaConfigDemoApp {
+	public static void main(String[] args) {
+		// ClassPathXmlApplicationContext context = new
+		// ClassPathXmlApplicationContext("applicationContext.xml");
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SportConfig.class);
+		// get the bean from spring container
+		Coach theCoach = context.getBean("tennisCoach", Coach.class);
+		// call a method on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		System.out.println(theCoach.getDailyFortune());
+		// Close the context
+		context.close();
+	}
 }
